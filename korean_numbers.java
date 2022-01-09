66bd2dc758e22a30a2d35a2e4c0195c61c22f816
Katelyn Valete <katelynvalete@Katelyns-MBP.lan>
2022-01-09 12:23:12 -0500

    added sinoNumbers.txt

--- korean_numbers.java
+++ korean_numbers.java
@@ -1,1 +1,10 @@
-System.out.println("Hello");
+import java.util.HashMap;
+
+public class Korean_Numbers {
+
+  HashMap<Integer, String> sinoMap = new HashMap<>();
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
